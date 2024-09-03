b2bc82f94ee82420a20bdc7fb1659e0e08936c7c
Bryan Al Hilal Siregar <bryanalhilalsiregar@mail.ugm.ac.id>
2024-09-03 15:07:45 +0700

    Add files via upload

---
+++ Tugas Chapter 2/test_cases.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+	/* Test Problem 1: Payslip problem
+		input format: name, salary, installment, insurance
+		
+		["Adi", 1000000, 100000, 100.000] --> 
+		Payslip for Employee
+		---------------------
+		Name: Adi
+		Gross Salary: Rp1.000.000,00
+		Tax (20%): Rp200.000,00
+		Installment: Rp100.000,00
+		Insurance: Rp100.000,00
+		Net Salary: Rp600.000,00
+		["Bryan", 0, 0, 0] -->
+		Payslip for Employee
+		---------------------
+		Name: Bryan
+		Gross Salary: Rp0,00
+		Tax (20%): Rp0,00
+		Installment: Rp0,00
+		Insurance: Rp0,00
+		Net Salary: Rp0,00
+		
+		["Caca", 1000000000000, 1, 1] -->
+		Payslip for Employee
+		---------------------
+		Name: Caca
+		Gross Salary: Rp1.000.000.000.000,00
+		Tax (20%): Rp200.000.000.000,00
+		Installment: Rp1,00
+		Insurance: Rp1,00
+		Net Salary: Rp799.999.999.998,00
+	
+	*/
+	/* Test Problem 2: Quadratic Problem
+	  input format: a, b ,c 
+	  
+	  jika dua output:
+	  output format: x_1, x_2
+	  
+	  jika satu output:
+	  output format: x
+	  
+	  jika tidak ada output:
+	  output format: Tidak ada
+	  
+	  [2, -1, -15] --> [3, -2.5]
+	  [1, 2, 5] --> [Tidak ada]
+	  [1, -4, 4] --> [2]
+	*/ 
+	
+}
