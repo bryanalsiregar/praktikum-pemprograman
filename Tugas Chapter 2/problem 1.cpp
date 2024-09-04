19e50c5a9831746525b29662906a1da5f693a900
SynitCool <zolarpixel3@gmail.com>
2024-09-04 13:05:46 +0700

    Perubahan

--- Tugas Chapter 2/problem 1.cpp
+++ Tugas Chapter 2/problem 1.cpp
@@ -1,19 +1,32 @@
-#include<iostream>
-#include<string>
-#include<algorithm>
+/*
+	File ini digunaka untuk menyelesaikan problem 1: Payslip Calculation
+	Dijalankan di onlinegdb.com
+*/
+// Header yang digunakan untuk file ini
+#include<iostream> // Untuk penggunaan I/O di C++
+#include<string> // Untuk penggunaan tipe string dan manipulasinya
+#include<algorithm> // Untuk menggunakan algoritma yang sudah dibuat sebelumnya
 
+// Penggunaam scope std
 using namespace std;
 
+// Penamaan kembali tipe untuk penyederhanaan
 typedef unsigned long long ull;
 
+
+// Fungsi ini untuk membnatu dalam pemisahan ribuan dengan penambahan titik dengan output tipe string
 string thousandSeparator(int n) 
 { 
+	// Inisial output
     string ans = ""; 
-  
+	
+	// Mengubah variabel n yang int ke dalam string
     string num = to_string(n); 
-  
+
+	// Melakukan penambahan titik koma setelah tiga digit dari belakang angka
+	// Dilakukan counting untuk mencari tahu apakah sudah pada digit ke-3 dari belakang
+    // Penambahan titik koma dan tiga digit pada variabel ans
     int count = 0; 
-    
     for (int i = num.size() - 1; 
          i >= 0; i--) { 
         count++; 
@@ -24,21 +37,26 @@ string thousandSeparator(int n)
             count = 0; 
         } 
     } 
-  
+	
+	// Output harus dibalikkan sebab pencarian tiga digit belakang angka
     reverse(ans.begin(), ans.end()); 
   
-
+	// Pengecekkan apakah panjang variabel ans habis dibagi 4 
+	// Jika iya maka terjadi penghapusan pada variabel ans
     if (ans.size() % 4 == 0) { 
         ans.erase(ans.begin()); 
     } 
-  
+	
+	// Keluarkan output
     return ans; 
 } 
 
 int main(){
+	// Deklarasi variabel
 	string name;
 	ull salary, installment, insurance, tax;
 	
+	// Penggunaa prompt dan input
 	cout << "Input your name" << endl;
 	cin >> name;
 	cout << "Input your gross salary cost" << endl;
@@ -48,7 +66,10 @@ int main(){
 	cout << "Input your insurance cost" << endl;
 	cin >> insurance;
 	
+	// Penghitungan tax yang harus dibayar
 	tax = salary*0.2;
+
+	// Output dalam terminal
 	cout << " " << endl;
 	cout << " " << endl;
 	cout << " " << endl;
--- Tugas Chapter 2/problem 2.cpp
+++ Tugas Chapter 2/problem 2.cpp
@@ -1,24 +1,40 @@
-#include<iostream>
-#include<math.h>
+/*
+	File ini berisi mengenai penyelesaian pada problem 2: Quadratic Equation Solver
+	Dijalankan di onlinegdb.com
+*/
 
+// Header file yang digunakan
+#include<iostream> // Untuk mengatur I/O C++
+#include<math.h> // Untuk menggunakan fungsi matematika yang tidak ada pada default C++
+
+// Pengguanaan scope std
 using namespace std;
 
 int main() {
+	// Deklarasi variabel yang dibutuhkan
 	float a, b, c, disc, x_1, x_2;
+	// Input untuk mengambil data dari user
 	cin >> a >> b >> c;
 	
+	// Penghitungan diskriminan
 	disc = pow(b,2)-4*a*c;
 	
+	// Kasus jika diskriminan lebih dari 0
 	if (disc > 0) {
+		// Output pada terminal tentang real roots
 		cout << "there are two distinct real root: " << endl;
 		cout << "x_1: " << (-b+sqrt(disc))/(2*a) << endl;
 		cout << "x_2: " << (-b-sqrt(disc))/(2*a) << endl;
 	}
+	// Kasus Jika diskriminan sama dengan 0
 	if (disc == 0) {
+		// Output pada terminal tentang real roots
 		cout << "there are exactly one real root: " << endl;
 		cout << "x: " << (-b)/(2*a) << endl;
 	}
+	// Kasus jika diskriminan kurang dari 0
 	if (disc < 0) {
+		// Ouput pada terminal tentang real roots
 		cout << "there are no real roots (the roots are complex)" <<endl;
 	}
 	
