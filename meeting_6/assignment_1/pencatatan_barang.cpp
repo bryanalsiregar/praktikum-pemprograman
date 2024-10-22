59037ac97bd177808a4ab424869dc391f45580ef
Bryan Al Hilal Siregar <bryanalhilalsiregar@mail.ugm.ac.id>
2024-10-22 06:32:57 +0000

    perbaiki tipis2

--- meeting_6/assignment_1/pencatatan_barang.cpp
+++ meeting_6/assignment_1/pencatatan_barang.cpp
@@ -5,23 +5,24 @@
 
 #include<iostream> // untuk menggunakan input dan ouput pada terminal 
 #include<string> // untuk menggunakan tipe data string
-#include<fstream>
-#include<sstream>
+#include<fstream> // untuk membuka file test.txt
+#include<sstream> // untuk mendapatkan kata dalam string
 
 using namespace std; // untuk menyederhanakan std
 
 // Identitas setiap barang
 struct barang {
-    string kode_barang;
-    string nama_barang;
-    int harga_barang;
-    int jumlah_barang;
+    string kode_barang; // kode barang
+    string nama_barang; // nama barang
+    int harga_barang; // harga barang
+    int jumlah_barang; // jumlah barang
 };
 
 string pencatatan_barang(int n, barang barang_milik[]) {
     /*
         Fungsi ini digunakan untuk menyelesaikan masalah pencatatan barang.
         int n: banyaknya barang yang tersedia
+        barang[] barang_milik: barang-barang yang tersedia dalam array
     */
 
     // cek jika barang yang dimiliki melampaui kapasitas
@@ -59,6 +60,15 @@ int run_func() {
     // digunakan untuk menginput banyaknya barang pada terminal
     cin >> n;
 
+    if (n > 50) {
+        cout << "Barang melampaui kapasitas!" << endl;
+        return 0;
+    // cek jika input banyaknya barang tidak valid
+    } else if (n < 0) {
+        cout << "Banyaknya barang tidak valid!" << endl;
+        return 0;
+    }
+
     // untuk menampung barang yang diinputkan
     barang barang_milik[n];
 
@@ -87,8 +97,8 @@ int test_func() {
     int n_input; // untuk menampung input banyaknya barang pada test.txt
     string input; // untuk menampung anggota vektor pada baris test.txt 
     int count = 0; // indikasi apakah baris tersebut merupakan input atau output
-    barang barang_input[999];
-    int count_barang = 0;
+    barang barang_input[999]; // untuk menampung barang dalam test.txt
+    int count_barang = 0; // untuk menghitung banyaknya barang dalam tiap test
 
     // periksa apakah file test.txt dapat dibuka
     if (!testFile) {
@@ -107,34 +117,50 @@ int test_func() {
         if (count == 0) { // indikasi jika input
             // inisialisasi baris
             n_input = stoi(line);
+            // cek jika n kurang dari 0 atau lebih dari 0
             if (n_input < 0 || n_input > 50) {
+                // menuju ke indikasi output
                 count = 2;
             } else {
                 // mengubah indikasi
                 count++;
             } 
-        } else if (count == 1) {
+        } else if (count == 1) { // indikasi jika input barang
+            // indikasi identitas barang
             int count_iden_barang = 0;
+            // inisialisasi baris pada test
             istringstream iss(line);
+            // pengulangan kata dalam baris
             while (iss >> input) {
+                // cek jika kata dalam baris itu suatu identitas
                 switch (count_iden_barang) {
+                    // identitas kode barang
                     case 0: barang_input[count_barang].kode_barang = input; break;
+                    // identitas nama barang
                     case 1: barang_input[count_barang].nama_barang = input; break;
+                    // identitas harga barang
                     case 2: barang_input[count_barang].harga_barang = stoi(input); break;
+                    // indetitas jumlah barang
                     case 3: barang_input[count_barang].jumlah_barang = stoi(input); break;
+                    // jika tidak memenuhi pengecekkan
                     default: break;
                 }
+                // pengubahan identitas kata dalam baris
                 count_iden_barang++;
-            } 
+            }
+            // menghitung barang yang sudah masuk
             count_barang++;
+            // jika barang sudah terpenuhi semua
             if (count_barang == n_input) {
+                // kembali untuk menghitung dari tidak ada barang
                 count_barang = 0;
+                // ubah indikasi
                 count++;
             }
         } else if (count == 2) { // indikasi jika output
-            // melakukan reverse pada input
+            // melakukan pencatatan barang pada input
             string output = pencatatan_barang(n_input, barang_input);
-            // periksa apabila output pada fungsi reverse_array sama dengan output yang diinginkan di test.txt
+            // periksa apabila output pada fungsi pencatatan_barang sama dengan output yang diinginkan di test.txt
             if (output == line) {
                 // Jika sama dengan, tampilkan ini
                 cout << "Test " << test_num << " passed!" << endl;
@@ -147,7 +173,7 @@ int test_func() {
             }
             test_num++; // lanjut ke tes selanjutnya
             count = 0; // kembali ke mode input
-            count_barang = 0;
+            count_barang = 0; // kembali menghitung barang
         } 
     }
 
