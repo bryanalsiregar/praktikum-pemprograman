d58968076ff39b633e0a1573fd07124c59e3372d
Bryan Al Hilal Siregar <bryanalhilalsiregar@mail.ugm.ac.id>
2024-11-12 07:55:08 +0000

    comment hitung_rata_rata

--- meeting_8/assignment_2/hitung_rata_rata.cpp
+++ meeting_8/assignment_2/hitung_rata_rata.cpp
@@ -1,21 +1,32 @@
-#include<iostream>
+/*
+    File ini digunakan untuk menyelesaikan permasalahan hitung rata rata
+*/
+
+#include<iostream> // untuk mengatur input dan output pada terminal
 #include<string> // untuk menggunakan tipe data string
 #include<fstream> // untuk membuka file test.txt
 #include<sstream> // untuk mendapatkan kata dalam string
 
-using namespace std;
-/*
- 1 2 3
- 1+2+3/3 = 2
 
- f(x, y=0, n) = {y, n = 0
-                {y/len(x), n = 1
-                {
-                {f(x, y+x[n-1], n-1), n > 1
-                
-*/
+// untuk menyingkat pemanggilan fungsi standar
+using namespace std;
 
 float hitung_rata_rata(float x[], float y, int n, int i) {
+    /*
+        Fungsi ini digunakan untuk menyelesaikan masalah hitung rata-rata
+        dengan penjelasan sebagai berikut
+
+        Ilustrasi permasalan
+        1 2 3
+        1+2+3/3 = 2
+
+        Dari ilustrasi dapat disimpulkan seperti ini 
+        f(x, y=0, n), i) = {-1, n < 0
+                           {0, n = 0
+                           {(y+x[i])/n, i = 0
+                           {f(x, y+x[i], n, i-1), n > 1
+    */
+    
     if (n < 0) return -1;
     if (n == 0) return 0;
 
@@ -28,14 +39,19 @@ int run_func() {
     /*
         Fungsi ini digunakan untuk menjalankan program tanpa test.txt
     */
+    // mengambil input n dari pengguna
     int n;
     cin >> n;
+    
+    // mengambil input larikan dari pengguna dari terminal
     float x[n];
     for (int i = 0; i < n; i++) {
         cin >> x[i];
     }
+    // menghitung larikan dan n untuk fungsi hitung_rata_rata
     float r = hitung_rata_rata(x, 0, n, n-1);
 
+    // cetak hasil dari fungsi hitung_rata_rata
     cout << r << endl;
     return 0;
 }
@@ -44,16 +60,13 @@ int test_func() {
     /*
         Fungsi ini digunakan untuk menjalankan program dengan test.txt
     */
-    /*
-        Fungsi ini digunakan untuk menjalankan program dengan test.txt
-    */
     ifstream testFile("test.txt"); // inisialialisi pembukaan file test.txt
     string line; // untuk menampung baris pada test.txt
     string expected_output; // untuk menampung output pada test.txt
-    int n_input; // untuk menampung input banyaknya barang pada test.txt
-    string input; // untuk menampung anggota vektor pada baris test.txt 
+    int n_input; // untuk mengambil banyaknya elemen pada larikan dari input
+    string input; // untuk mengambil input test.txt 
     int count = 0; // indikasi apakah baris tersebut merupakan input atau output
-    string output;
+    string output; // inisiasi output untuk fungsi hitung_rata_rata
 
     
 
@@ -72,11 +85,14 @@ int test_func() {
     while (getline(testFile, line)) {
         // indikasi jika baris adalah input atau output
         if (count == 0) { // indikasi jika input
-            // inisialisasi baris
+            // mengambil banyaknya elemen pada larikan di test.txt
             n_input = stoi(line);
+            // ubah indikasi
             count++;
-        } else if (count == 1) {
+        } else if (count == 1) { // indikasi jika merupakan input larikan
+            // jika larikan valid
             if (n_input > 0) {
+                // iniasiasi input dan baris pada test.txt
                 float x[n_input];
                 int i = 0;
                 istringstream iss(line);
@@ -84,20 +100,26 @@ int test_func() {
                     x[i] = stoi(input);
                     i++;
                 }
-                // melakukan pencatatan barang pada input
+                // menghitung rata-rata dengan fungsi hitung_rata_rata
                 output = to_string(hitung_rata_rata(x, 0, n_input, n_input-1));
             }
+            // ubah indikasi
             count++;
-        } else if (count == 2) { // indikasi jika input barang
+        } else if (count == 2) { // indikasi jika output barang
+            // jika banyaknya elemen larikan adalah 0
             if (n_input == 0) {
+                // menjalankan fungsi dengan hitung_rata_rata
                 float x[n_input];
                 float r = hitung_rata_rata(x, 0, n_input, n_input-1);
 
                 output = to_string(r);
+            // jika banyaknya elemen larikan kurang dari 0
             } else if (n_input < 0) {
+                // menjalankan fungsi dengan hitung_rata_rata
                 float x[n_input];
                 float r = hitung_rata_rata(x, 0, n_input, n_input-1);
 
+                // cetak jika valid atau tidak
                 if (r == -1) {
                     output = "Tidak Valid!";
                 } else {
@@ -125,6 +147,10 @@ int test_func() {
 }
 
 int main(int argc, char *argv[]) {
+    /*
+        Fungsi utama main digunakan untuk mengecek apakah pengguna
+        ingin menjalankan program dalam mode tes atau manual
+    */
     if (argc > 1 && string(argv[1]) == "test") {
         test_func();
     } else {
