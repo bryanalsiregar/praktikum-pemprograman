c0b9f6644f8c91dd58a4b339250cc0f7f1c82282
Bryan Al Hilal Siregar <bryanalhilalsiregar@mail.ugm.ac.id>
2024-11-13 11:50:38 +0000

    remove typo

--- meeting_8/assignment_2/hitung_rata_rata.cpp
+++ meeting_8/assignment_2/hitung_rata_rata.cpp
@@ -21,7 +21,7 @@ float hitung_rata_rata(float x[], float y, int n, int i) {
         1+2+3/3 = 2
 
         Dari ilustrasi dapat disimpulkan seperti ini 
-        f(x, y=0, n), i) = {-1, n < 0
+        f(x, y=0, n, i) = {-1, n < 0
                            {0, n = 0
                            {(y+x[i])/n, i = 0
                            {f(x, y+x[i], n, i-1), n > 1
